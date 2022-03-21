b2eb4b1f999f34b44f4b738d36f23ebdd3399e9e
ShadowFlutter <102014278+ShadowFlutter@users.noreply.github.com>
2022-03-21 01:33:00 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -3,7 +3,7 @@ $destino= "loquendo200@outlook.com";
 $nombre = $_POST["nombre"];
 $correo = $_POST["correo"];
 $telefono = $_POST["telefono"];
-$mensaje = $_POST["mensaje"]
+$mensaje = $_POST["mensaje"];
 $contenido = "Nombre: " . $nombre . "\nCorreo " . $correo . "\nTelefono: " . $telefono . "\nMensaje" . $mensaje;
 mail($destino, "Contacto", $contenido);
 header("Location:gracias.html");
